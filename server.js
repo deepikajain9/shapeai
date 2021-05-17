79460e69eba3addc1785b0c75c15b4157933bab6
deepika jain <70093491+deepikajain9@users.noreply.github.com>
2021-05-17 16:19:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(3000,function() {
+      console.log("server is started");
+})
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.post("/",function(req ,res){
+     var n1 = Number (req.body.number1);
+     var n2 = Number (req.body.number2);
+     var result = n1/(n2*n2);
+     res.send("The Answer is : "+ result);
+});
